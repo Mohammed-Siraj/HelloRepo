c214f0de82cc6b12c852b15af46ca5438b91e6f5
Ahmed <dontknow.itsme@gmail.com>
2013-09-09 14:19:28 +0530

    Checking the Commit

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -3,6 +3,7 @@ package test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.print("\n" + "Hello World");
+		System.out.print("\n" + "Hello World, Checking the commit");
+		
 	}
 }
